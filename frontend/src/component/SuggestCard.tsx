2b316448f0ad05d75499a9e380251d80d8b788c0
Ivan <ivan.munoz.50309277@gmail.com>
2025-07-06 15:52:50 +0800

    Completed UI

--- frontend/src/component/SuggestCard.tsx
+++ frontend/src/component/SuggestCard.tsx
@@ -1,5 +1,34 @@
+import "./SuggestCard.css";
+
+import Gemini from "../assets/gemini.png";
+import GeminiIcon from "../assets/geministart.png";
+import { RotateCcw, Sun, Wind, Cloud, Droplets } from "lucide-react";
+
 const SuggestCard = () => {
-  return <div className="suggest-card"></div>;
+  return (
+    <div className="suggest-card">
+      <button className="refresh">
+        <RotateCcw size={18} color="white" />
+      </button>
+      <img src={Gemini} alt="" width={90} />
+      <div className="suggestion-title">
+        <h4>Outfit Suggestion by Gemini:</h4>
+      </div>
+      <div className="suggestion-box">
+        <ul>
+          <li>Rain boots, leggings, oversized sweater, rain jacket.</li>
+          <li>Waterproof sneakers, jeans, t-shirt, hoodie, trench coat.</li>
+          <li>
+            Chelsea boots, dress pants, button-down shirt, light raincoat.
+          </li>
+          <li>Loafers, midi skirt, turtleneck, cardigan, umbrella.</li>
+          <li>
+            Ankle boots, corduroy pants, long-sleeved shirt, puffer vest, hat.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default SuggestCard;
--- frontend/src/component/WeatherCard.tsx
+++ frontend/src/component/WeatherCard.tsx
@@ -3,40 +3,49 @@ import Forecast from "../assets/forecast.png";
 
 import { RotateCcw, Sun, Wind, Cloud, Droplets } from "lucide-react";
 
+import CloudyGif from "../assets/cloudy.gif";
+import SunnyGif from "../assets/sunny.gif";
+import ThunderstromGif from "../assets/thunderstom.gif";
+import RainyGif from "../assets/rainy.gif";
+import WindyGif from "../assets/windy.gif";
+
+import "./WeatherCard.css";
+
 const WeatherCard = () => {
   return (
     <div className="weather-card">
+      <img src={ThunderstromGif} alt="weather gif" className="background-gif" />
       <button className="refresh">
-        <RotateCcw size={20} />
+        <RotateCcw size={18} color="white" />
       </button>
       <div className="forecast-container">
         <img className="forecast" src={Forecast} alt="" />
       </div>
       <h1>Manila, Philippines</h1>
       <h2>Cloudy</h2>
-      <img src={Cloudy} alt="" />
+      <img className="weather-logo" src={Cloudy} alt="" />
       <h1>78°C</h1>
       <h2>12:30 AM</h2>
       <div className="current-container">
         <div>
           <Wind size={20} />
           <h3>90</h3>
-          <p>wind_kph</p>
+          <p>Wind_kph</p>
         </div>
         <div>
           <Droplets size={20} />
           <h3>90</h3>
-          <p>humidity</p>
+          <p>Humidity</p>
         </div>
         <div>
           <Cloud size={20} />
           <h3>90</h3>
-          <p>cloud</p>
+          <p>Cloud</p>
         </div>
         <div>
           <Sun size={20} />
           <h3>90C</h3>
-          <p>heatindex_c</p>
+          <p>Heatindex</p>
         </div>
       </div>
     </div>
